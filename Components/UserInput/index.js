367fe7ac78d3bec23aa317a6efcf542e29428a67
Lorcan Mc <lorcan.jmc@gmail.com>
2022-02-18 14:15:42 +0000

    userInput

--- Components/UserInput/index.js
+++ Components/UserInput/index.js
@@ -1,24 +1,38 @@
-import React, {useState} from 'react';
+import { useState } from "react";
 
 const UserInput = () => {
-    const [text, setText] = useState("");
+  const API_URL = "https://ukla16.herokuapp.com";
+  const [text, setText] = useState("");
+  console.log(text);
 
-    function onSubmit () {
-        
-    }
-
-    function handleChange (e) {
-        setText(e.target.value)
-    }
+  async function onSubmit(postText) {
+    const postObj = {
+        familyID: 1,
+        userID: 3,
+        username: "Alan",
+        postText: "some string",
+        createdAt: "2019-10-04T12:00:17.000Z"
+    };
     
+    const res = await fetch(`${API_URL}/posts`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(postObj),
+    });
+    const result = await res.json()
+    console.log(result)
+  }
 
-    return (
-        <div>
-            <input type="text" onChange={handleChange} />
-            <button onClick={onSubmit} />
-        </div>
-    
-    )
-}
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+      <button onClick={() => onSubmit(text)}>Submit</button>
+    </div>
+  );
+};
 
 export default UserInput;
