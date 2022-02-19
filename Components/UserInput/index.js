90ae87765063ca9ef0321897ce7a65db0acc3956
Lorcan Mc <lorcan.jmc@gmail.com>
2022-02-19 23:27:39 +0000

    post request working

--- Components/UserInput/index.js
+++ Components/UserInput/index.js
@@ -1,26 +1,26 @@
 import { useState } from "react";
 
 const UserInput = () => {
-  const API_URL = "https://ukla16.herokuapp.com";
+  const API_URL = "http://localhost:3001";
   const [text, setText] = useState("");
   console.log(text);
 
   async function onSubmit(postText) {
-      const postObj = {
-        familyID: 1,
-        userID: 3,
-        username: "Alan",
-        postText: "some string",
-        createdAt: "2019-10-04T12:00:17.000Z"
-      };
-  
-      const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(postObj),
-      };
-  
-      const res = await fetch(`${API_URL}/posts`, requestOptions);
+    const postObj = {
+      family_id: 1,
+      user_id: 3,
+      user_name: "Alan",
+      post_text: postText,
+      created_at: "2019-10-04T11:00:17.000Z",
+    };
+
+    const res = await fetch(`${API_URL}/posts`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(postObj),
+    });
+    const data = await res.json();
+    console.log(data);
   }
 
   function handleChange(e) {
