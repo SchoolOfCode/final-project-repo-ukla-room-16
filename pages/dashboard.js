4f3edf69aab35f5b19edeb116341823dd672aaaf
Najmadiiriye <najmadiiriye11@gmail.com>
2022-02-21 14:14:26 +0000

    pages updated

---
+++ pages/dashboard.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import UserInput from '../component/UserInput';
+import Feed from '../component/Feed'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+      <UserInput />
+      <Feed />
+    </div>
+  )
+};
