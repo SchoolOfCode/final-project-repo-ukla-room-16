24d0ca3d0b41e5442d2a004500f97a6cc8733ba0
kevweolomu <kevweolomu@yahoo.ca>
2022-02-21 15:43:05 +0000

    user-profile created

---
+++ pages/user.js
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+
+export default function User(){
+    return (
+        <div className="user-container">
+            <div className="profile">
+                <img src="" alt="user-profile-picure" />
+                <header></header>
+                <p></p>
+            </div>
+
+            <div className="favourite-food">
+                <img src="" alt="favourite-food" />
+                <p></p>
+            </div>
+
+            <div className="medicals">
+                <img src="" alt="medical-info" />
+                <p></p>
+            </div>
+
+            <div className="clothes-sizes">
+                <img src="" alt="clothes-size" />
+                <p></p>
+            </div>
+
+            <div className="shoe-size">
+                <img src="" alt="shoe-size" />
+                <p></p>
+            </div>
+        </div>
+    )
+}
