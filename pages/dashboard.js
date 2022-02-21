7cff854610a4cadab3337f0538504cbaa6f8b50e
marian.eq <mariano.eq@gmail.com>
2022-02-21 12:18:29 -0300

    newBranchAdded

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,15 +1,16 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import UserInput from '../component/UserInput';
-import Feed from '../component/Feed'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import UserInput from "../component/UserInput";
+import Feed from "../component/Feed";
 
 export default function Dashboard() {
   return (
     <div>
+      <img src="../images/25856.jpg" alt="" />
       <h1>Hello World!!!</h1>
-      <UserInput />
-      <Feed />
+      {/* <UserInput />
+      <Feed /> */}
     </div>
-  )
-};
+  );
+}
