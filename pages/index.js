2a2e2c0008a22e078b4e458d538ea6b125b222c8
Tonisaraiva <scramtoni@gmail.com>
2022-02-21 15:09:01 +0000

    landing page design with picture and name

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,24 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import UserInput from '../component/UserInput';
-import Feed from '../component/Feed'
+import frontimage from "../images/familia_frontpage_image.jpg"
 
-export default function Dashboard() {
+export default function Home() {
   return (
     <div>
-      <h1>Hello World!!!</h1>
-      <UserInput />
-      <Feed />
+      <h1>Familia</h1>
+      
+
+      <Image
+      src={frontimage}
+      alt="Picture of a family from grandparents, to children and grandchildren"
+      width="700em"
+      height="700em"
+    />
+
+
+
+
     </div>
   )
 };
