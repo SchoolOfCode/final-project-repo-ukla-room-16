00b4d55ebb991ef9cfe54cd31efade2bdbfa67a7
Tonisaraiva <scramtoni@gmail.com>
2022-02-21 10:11:54 +0000

    with feed in

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import UserInput from '../Components/UserInput';
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World!!!</h1>
       <UserInput />
+      <Feed />
     </div>
   )
 };
