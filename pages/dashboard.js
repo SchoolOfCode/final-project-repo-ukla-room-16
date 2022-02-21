cf978c0ee28e79b3c8c874ed29782d0d274f46ce
marian.eq <mariano.eq@gmail.com>
2022-02-21 13:23:55 -0300

    Dashboard style in progress

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,40 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Dashboard.module.css";
 import UserInput from "../component/UserInput";
 import Feed from "../component/Feed";
+import landingPic from "../images/25856.jpg";
+import addIcon from "../images/icons8-add-100.png";
 
 export default function Dashboard() {
   return (
     <div>
-      <img src="../images/25856.jpg" alt="" />
-      <h1>Hello World!!!</h1>
-      {/* <UserInput />
-      <Feed /> */}
+      <header className={styles.header}></header>
+      <div className={styles.container}>
+        <div className={styles.dashboard}>
+          <Image src={landingPic} />
+        </div>
+        <div>
+          <UserInput />
+          <Feed />
+        </div>
+        <div>
+          <input
+            type="text"
+            className={styles.searchBar}
+            placeholder="Search bar"
+          />
+          <div className={styles.tree}>
+            <p>FAMILY TREE</p>
+          </div>
+          <div className={styles.events}>
+            <button>
+              <Image src={addIcon} />
+            </button>
+            <p>EVENTS</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
