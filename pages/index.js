ab6e3f63a709c55b645ab1a8a86d927728968256
Tonisaraiva <scramtoni@gmail.com>
2022-02-22 05:40:31 +0000

    with auth0 working

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,32 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import frontimage from "../images/familia_frontpage_image.jpg"
+import logofamilia from "../images/Familia.png"
 import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
+const {user, error, isLoading} = useUser();
+console.log(user);
+if (isLoading) return <div>...loading</div>
+if (error) return <div>{error.message}</div>
+if (user){
+  return (
+    <>
+      <h1> Welcome {user.name}!</h1>
+      <a href="/api/auth/logout">Logout</a>
+    </>
+  )
+}
+
   return (
     <div>
      <h1 className={styles.title}>Familia</h1>
+      <Image src={logofamilia}
+      alt="logo saying Familia, the name of our app"
+      className={styles.logo} 
+      
+      />
       
 
       <Image
@@ -17,7 +37,7 @@ export default function Home() {
       width="1000em"
       height="500em"
     />
-{/* <a href="/api/auth/login">Login</a> */}
+<a href="/api/auth/login">Login</a>
 <button className={styles.signup}> Sign Up</button>
 <button className={styles.login}> Login</button>
 
