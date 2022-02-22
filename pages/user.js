3a01f5e9de7e2ff4a553493f855fa0108c4ffde7
kevweolomu <kevweolomu@yahoo.ca>
2022-02-22 10:19:58 +0000

    Css flex box and ither stylings done

--- pages/user.js
+++ pages/user.js
@@ -1,36 +1,37 @@
 import Head from 'next/head';
-import Css from '../styles/user.module.css';
+import css from '../styles/user.module.css';
 
 
 export default function User(){
     return (
-        <div className="user-container">
-            <div className="profile">
+        <div className={css.container}>
+            <div className={css.profile}>
                 <img src="https://www.pngitem.com/pimgs/m/90-906935_animated-faces-my-hero-design-clip-art-woman.png" alt="user-profile-picure" />
                 <h1> Username </h1>
-                <h1> Dob </h1>
+                <h2> Dob </h2>
                 <p> email address </p>
                 <p> mobile number </p>
                 <p> home address </p>
             </div>
 
-            <div className="favourite-food">
+            <div className={css.favouriteFood}>
                 <img src="https://ak.picdn.net/shutterstock/videos/1085657018/thumb/11.jpg?ip=x480" />
                 <h6> Favourite Food: </h6>
-                <ul>
+                {/* <ul>
                     <li>
 
                     </li>
-                </ul>
+                </ul> */}
             </div>
 
-            <div className="medicals">
+            <div className={css.medicals}>
                 <img src="https://media.istockphoto.com/vectors/medic-stethoscope-concept-logotype-template-design-business-logo-icon-vector-id1200712144?k=20&m=1200712144&s=612x612&w=0&h=sNMhJLCryisznj072aZ2TeSDSi90w3PJ8NuUR8ErxaM=" alt="medical-info" />
                 <h6> Blood type: o- </h6>
+              
                 <h6> Allergies: No allergies </h6>
             </div>
 
-            <div className="clothes-sizes">
+            <div className={css.clothesSize}>
                 <img src="https://thumbs.dreamstime.com/b/clothes-logo-template-vector-icon-design-181127120.jpg" alt="clothes-size" />
                 <h6> Clothes size </h6>
                 <p>
@@ -46,7 +47,7 @@ export default function User(){
                 </p>
             </div>
 
-            <div className="shoe-size">
+            <div className={css.shoeSize}>
                 <img src="https://i.pinimg.com/550x/de/13/be/de13bedba8ab6783e2e460ff0e58e0a5.jpg" alt="shoe-size" />
                <h6> Shoe size </h6>
                 <p>
