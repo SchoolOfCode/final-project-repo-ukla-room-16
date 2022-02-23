99cb21d5ab47c1089529116bab81a708da61c95e
marian.eq <mariano.eq@gmail.com>
2022-02-23 08:49:01 -0300

    images have changed - page re-designed according to Figma

--- pages/user.js
+++ pages/user.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import css from "../styles/user.module.css";
 import Image from "next/image";
-import meal from "../images/meal.jpeg";
+import meal from "../images/meal.png";
 import userProfile from "../images/userProfile.svg";
-import medicals from "../images/medicals.jpeg";
-import clothes from "../images/clothes.jpeg";
-import shoes from "../images/shoes-icon.jpeg";
+import medical from "../images/medical.png";
+import clothes from "../images/clothes.png";
+import shoes from "../images/shoes.png";
 
 export default function User() {
   return (
@@ -20,27 +20,27 @@ export default function User() {
 
         <div className={css.info}>
           <div className={css.subInfo}>
-            <Image src={meal} width="159px" height="121px" />
+            <Image src={meal} width="85px" height="90px" />
             <ul>
               <p>Favourite foods: Sushi - Pasta.</p>
             </ul>
           </div>
           <div className={css.subInfo}>
-            <Image src={medicals} width="159px" height="121px" />
+            <Image src={medical} width="80px" height="50px" />
             <ul>
               <p>Blood type: 0-</p>
               <p>Allergies: None</p>
             </ul>
           </div>
           <div className={css.subInfo}>
-            <Image src={clothes} width="159px" height="121px" />
+            <Image src={clothes} width="80px" height="90px" />
             <ul>
               <p>Upper-Body: Medium</p>
               <p>Lower-Body: 33 cm</p>
             </ul>
           </div>
           <div className={css.subInfo}>
-            <Image src={shoes} width="159px" height="121px" />
+            <Image src={shoes} width="80px" height="90px" />
             <ul>
               <p>US: 7</p>
               <p>UK: 6</p>
