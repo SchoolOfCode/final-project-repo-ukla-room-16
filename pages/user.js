3585ea468b1279a82bad3ed56bcdbf737737b1d6
marian.eq <mariano.eq@gmail.com>
2022-02-23 01:02:17 -0300

    new user profile pic/all code re-factored,looking like figma. Missing vertical white line as background

--- pages/user.js
+++ pages/user.js
@@ -1,71 +1,54 @@
-import Head from 'next/head';
-import css from '../styles/user.module.css';
-import Image from 'next/image';
-import meal from '../images/meal.jpeg';
-import profile from '../images/profile.jpeg';
-import medicals from '../images/medicals.jpeg';
-import clothes from '../images/clothes.jpeg';
-import shoes from '../images/shoes-icon.jpeg'
+import Head from "next/head";
+import css from "../styles/user.module.css";
+import Image from "next/image";
+import meal from "../images/meal.jpeg";
+import userProfile from "../images/userProfile.svg";
+import medicals from "../images/medicals.jpeg";
+import clothes from "../images/clothes.jpeg";
+import shoes from "../images/shoes-icon.jpeg";
 
+export default function User() {
+  return (
+    <div className={css.container}>
+      <div className={css.subContainer}>
+        <Image src={userProfile} width="259px" height="213px" />
+        <h2>User Name</h2>
+        <h3> DOB </h3>
+        <p> Email Address </p>
+        <p> Mobile Number </p>
+        <p> Home Address </p>
 
-
-export default function User(){
-    return (
-        <div className={css.container}>
-            <div className={css.profile}>
-                <Image src={profile} alt="user-profile" width="100px" height="100"/>
-                <h1> Username </h1>
-                <h2> Dob </h2>
-                <p> email address </p>
-                <p> mobile number </p>
-                <p> home address </p>
-            </div>
-<div className={css.info}>
-            <div className={css.favouriteFood}>
-                <Image src={meal} alt="favourite-meal" width="100px" height="100"/>
-                <h6> Favourite Food: </h6>
-                {/* <ul>
-                    <li>
-
-                    </li>
-                </ul> */}
-            </div>
-
-            <div className={css.medicals}>
-                <Image src={medicals} alt="medical-info"  width="100px" height="100"/>
-                <h6> Blood type: o- </h6>
-              
-                <h6> Allergies: No allergies </h6>
-            </div>
-
-            <div className={css.clothesSize}>
-                <Image src={clothes} alt="clothes-size"  width="100px" height="100" />
-                <h6> Clothes size </h6>
-                <p>
-                    <ul>
-                        <li>
-                            Upperbody: Medium 
-                        </li>
-                        <li>
-                            Lower-body: 33
-                        </li>
-                    </ul>
-
-                </p>
-            </div>
-
-            <div className={css.shoeSize}>
-                <Image src={shoes} alt="shoe-size" width="100px" height="100"/>
-               <h6> Shoe size </h6>
-                <p>
-                    <ul>
-                        <li> uk: 6 </li> 
-                        <li> us: 5 </li>
-                        <li> ue: 28cm </li>
-
-                    </ul>
-                </p>
-            </div>
+        <div className={css.info}>
+          <div className={css.subInfo}>
+            <Image src={meal} width="159px" height="121px" />
+            <ul>
+              <p>Favourite foods: Sushi - Pasta.</p>
+            </ul>
+          </div>
+          <div className={css.subInfo}>
+            <Image src={medicals} width="159px" height="121px" />
+            <ul>
+              <p>Blood type: 0-</p>
+              <p>Allergies: None</p>
+            </ul>
+          </div>
+          <div className={css.subInfo}>
+            <Image src={clothes} width="159px" height="121px" />
+            <ul>
+              <p>Upper-Body: Medium</p>
+              <p>Lower-Body: 33 cm</p>
+            </ul>
+          </div>
+          <div className={css.subInfo}>
+            <Image src={shoes} width="159px" height="121px" />
+            <ul>
+              <p>US: 7</p>
+              <p>UK: 6</p>
+              <p>UE: 28cm</p>
+            </ul>
+          </div>
         </div>
-  </div>  )
+      </div>
+    </div>
+  );
 }
