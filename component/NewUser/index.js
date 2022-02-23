0e527018d25f5f89fe405bb16e7fa2708d274bad
Wahida Omar <wahidaomar@Wahidas-MacBook-Air.local>
2022-02-23 13:08:01 +0000

    only new users, and not returning users to the users table

--- component/NewUser/index.js
+++ component/NewUser/index.js
@@ -1,27 +1,44 @@
-const NewUsers = ({user}) => {
+//PASSING IN THE PROP USERS (WITH JUST THE NAME AND EMAIL)
+const NewUsers = ({ name, email }) => {
   const URL = process.env.NEXT_PUBLIC_URL;
 
-  async function onLogin(name) {
+  async function onLogin(name,email) {
+
     const postObj = {
-        family_id: 1,
-      full_name: name
+      family_id: 1,
+      full_name: name,
+      email: email
     };
 
+    console.log(postObj)
+
+    //fetching the users table
     const res = await fetch(`${URL}/users`, {
-      method: "POST",
+      method: "GET",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(postObj),
     });
     const data = await res.json();
-    console.log(data);
+    console.log(data)
+
+
+    //CHECKING IF THAT USER EXISTS
+    const index = data.payload.findIndex(person => { return person.email === postObj.email})
+
+    //IF THAT USER DOES NOT EXIST, ADD THEM TO THE USERS TABLE
+      if(index === -1) {
+        async function postUsers() {
+          const res = await fetch(`${URL}/users`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(postObj),
+        })};
+        postUsers()
+      }
   }
 
-  onLogin(user)
+  onLogin(name, email);
 
-  return (
-    <div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default NewUsers;
--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,33 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import frontimage from "../images/familia_frontpage_image.jpg";
 import logofamilia from "../images/Familia2.png";
-import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0";
 import NewUsers from "../component/NewUser";
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
-  console.log(user);
 
   // waiting message on loading between pages
   if (isLoading) return <div>...loading</div>;
+
+
   //display error message in case of issue
   if (error) return <div>{error.message}</div>;
+
+
   // when user logs in say welcome with the name of the user.
   // The object returned when logging in has various keys which can be used
+
+
   if (user) {
     return (
       <>
-      <NewUsers user={user.name}/>
-      {console.log(user.name)}
+      {/* here we are passing down the name and email of the user as a prop */}
+      <NewUsers name={user.name} email={user.email}/>
+
         {/* here picking the full name of the user to display welcome message to */}
         <h1> Welcome {user.name}!</h1>
+
         {/* displaying a log out button under the welcome message */}
         <a href="/api/auth/logout">Logout</a>
       </>
@@ -59,9 +65,6 @@ export default function Home() {
             Login / Sign Up
           </a>
         </div>
-
-        {/* <button className={styles.signup}> Sign Up</button>
-<button className={styles.login}> Login</button> */}
       </div>
     </div>
   );
