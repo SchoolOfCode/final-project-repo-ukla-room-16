755c70177034ec0417aae7d71ca52d0ff9a5fa40
marian.eq <mariano.eq@gmail.com>
2022-02-24 07:08:01 -0300

    merging pages/index

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import frontimage from "../images/familia_frontpage_image.jpg";
-
 import logofamilia from "../images/Familia9.png";
 
 import { useUser } from "@auth0/nextjs-auth0";
@@ -15,20 +14,17 @@ export default function Home() {
   // waiting message on loading between pages
   if (isLoading) return <div>...loading</div>;
 
-
   //display error message in case of issue
   if (error) return <div>{error.message}</div>;
-
 
   // when user logs in say welcome with the name of the user.
   // The object returned when logging in has various keys which can be used
 
-
   if (user) {
     return (
       <>
-      {/* here we are passing down the name and email of the user as a prop */}
-      <NewUsers name={user.name} email={user.email}/>
+        {/* here we are passing down the name and email of the user as a prop */}
+        <NewUsers name={user.name} email={user.email} />
 
         {/* here picking the full name of the user to display welcome message to */}
         <h1> Welcome {user.name}!</h1>
