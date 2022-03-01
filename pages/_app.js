8597e6e6eb333ca944a5143ae7e543e3567e31f7
Wahida Omar <wahidaomar@Wahidas-MacBook-Air.local>
2022-03-01 16:14:11 +0000

    tree without react app

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
-import Head from 'next/head';
-import '../styles/globals.css'
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0';
-
+import Head from "next/head";
+import "../styles/globals.css";
+import React from "react";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-    <title>Familia</title>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-  </Head>
-{/* // makes auth0 available on every page */}
-  <UserProvider>
-  <Component {...pageProps} />
-</UserProvider>
-</>
-  )}
+      <Head>
+        <title>Familia</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {/* // makes auth0 available on every page */}
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </>
+  );
+}
 
-export default MyApp
+export default MyApp;
--- pages/tree.js
+++ pages/tree.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import App from '../src/app';
+import React from "react";
+// import App from "../familytree/src/components/App/index.js"
+// import App from "../component/FamilyTree/app.js"
+import FamilyTree from "../component/FamilyTree/mytree.js"
 
-export default function Tree () {
-    
-        return (
-            <div>
-                <App />
-            </div>
-        );
-            }
-    
-
-
+export default function Tree() {
+  return (
+    <div style={{height: '100%'}}>
+      <FamilyTree
+        nodes={[
+          {
+            id: 1,
+            mid:6,
+            pids: [2],
+            name: "Amber McKenzie",
+            gender: "female",
+            img: "https://cdn.balkan.app/shared/2.jpg",
+          },
+          {
+            id: 2,
+            pids: [1],
+            name: "Ava Field",
+            gender: "male",
+            img: "https://cdn.balkan.app/shared/m30/5.jpg",
+          },
+          {
+            id: 3,
+            mid: 1,
+            fid: 2,
+            name: "Peter Stevens",
+            gender: "male",
+            img: "https://cdn.balkan.app/shared/m10/2.jpg",
+          },
+          {
+            id: 4,
+            mid: 1,
+            fid: 2,
+            name: "Savin Stevens",
+            gender: "male",
+            img: "https://cdn.balkan.app/shared/m10/1.jpg",
+          },
+          {
+            id: 5,
+            mid: 1,
+            fid: 2,
+            name: "Emma Stevens",
+            gender: "female",
+            img: "https://cdn.balkan.app/shared/w10/3.jpg",
+          },
+          {
+            id: 6,
+            name: "Bah blh",
+            gender: "female",
+            img: "https://cdn.balkan.app/shared/w10/3.jpg",
+          },
+        ]}
+      />
+    </div>
+  );
+}
