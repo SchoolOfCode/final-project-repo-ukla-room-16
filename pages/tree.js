93de04ab3a446bce6eff2692de5098430919c7e0
Tonisaraiva <scramtoni@gmail.com>
2022-03-02 14:25:48 +0000

    development of tree and database attempts

--- pages/tree.js
+++ pages/tree.js
@@ -3,57 +3,25 @@ import React from "react";
 // import App from "../component/FamilyTree/app.js"
 import FamilyTree from "../component/FamilyTree/mytree.js"
 
+
 export default function Tree() {
+ 
+
   return (
     <div style={{height: '100%'}}>
       <FamilyTree
-        nodes={[
-          {
-            id: 1,
-            mid:6,
-            pids: [2],
-            name: "Amber McKenzie",
-            gender: "female",
-            img: "https://cdn.balkan.app/shared/2.jpg",
-          },
-          {
-            id: 2,
-            pids: [1],
-            name: "Ava Field",
-            gender: "male",
-            img: "https://cdn.balkan.app/shared/m30/5.jpg",
-          },
-          {
-            id: 3,
-            mid: 1,
-            fid: 2,
-            name: "Peter Stevens",
-            gender: "male",
-            img: "https://cdn.balkan.app/shared/m10/2.jpg",
-          },
-          {
-            id: 4,
-            mid: 1,
-            fid: 2,
-            name: "Savin Stevens",
-            gender: "male",
-            img: "https://cdn.balkan.app/shared/m10/1.jpg",
-          },
-          {
-            id: 5,
-            mid: 1,
-            fid: 2,
-            name: "Emma Stevens",
-            gender: "female",
-            img: "https://cdn.balkan.app/shared/w10/3.jpg",
-          },
-          {
-            id: 6,
-            name: "Bah blh",
-            gender: "female",
-            img: "https://cdn.balkan.app/shared/w10/3.jpg",
-          },
-        ]}
+        // nodes={[
+          
+        //   { id: 2, pids: [3], gender: 'male',photo: 'https://cdn.balkan.app/shared/w60/1.jpg', name:'Rowan Annable', born: '1952-10-10' },
+        //         { id: 3, pids: [2], gender: 'female', photo: 'https://cdn.balkan.app/shared/w60/1.jpg', name: 'Laura Shepherd', born: '1943-01-13', email: 'laura.shepherd@gmail.com', phone: '+44 845 5752 547', city: 'Moscow', country: 'ru' },
+        //         { id: 4, pids: [5], photo: 'https://cdn.balkan.app/shared/m60/3.jpg', name: 'Rowan Annable' },
+        //         { id: 5, pids: [4], gender: 'female', photo: 'https://cdn.balkan.app/shared/w60/3.jpg', name: 'Lois Sowle' },
+        //         { id: 6, mid: 2, fid: 3, pids: [7], gender: 'female', photo: 'https://cdn.balkan.app/shared/w30/1.jpg', name: 'Tyler Heath', born: '1975-11-12' },
+        //         { id: 7, pids: [6], mid: 5, fid: 4, gender: 'male', photo: 'https://cdn.balkan.app/shared/m30/3.jpg', name: 'Samson Stokes', born: '1986-10-01' },
+        //         { id: 8, pids: [9], mid: 7, fid: 6, gender: 'female', photo: 'https://cdn.balkan.app/shared/w10/3.jpg', name: 'Celeste Castillo', born: '2021-02-01' },
+        //         { id: 9, pids: [8], gender: 'male', photo: '', name: 'john', born: '2021-02-01' }
+           
+        // ]}
       />
     </div>
   );
