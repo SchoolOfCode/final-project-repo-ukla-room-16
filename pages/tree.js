0fcc5d95c874bd3a096942d7ea74aa0d1cea5a9e
Wahida Omar <wahidaomar@Wahidas-MacBook-Air.local>
2022-03-03 16:14:49 +0000

    tree working

--- pages/tree.js
+++ pages/tree.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { ourData } from "../component/FamilyTree/mytree.js";
-
+import FamilyTree, { ourData } from "../component/FamilyTree/mytree.js";
 
 export default function Tree() {
- 
-// here we are displaying the tree developped in the component folder under FamilyTree/mytree.js
+  // here we are displaying the tree developped in the component folder under FamilyTree/mytree.js
   return (
-    <div style={{height: '100%'}}>
-      <FamilyTree
-        nodes={
-            ourData
-        }
-      />
+    <div style={{ height: "100%" }}>
+      <FamilyTree nodes={ourData} />
     </div>
   );
 }
