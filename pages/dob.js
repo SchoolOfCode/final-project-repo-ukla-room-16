d80596ee0576ad09d0536541919f625e0e89b617
Tonisaraiva <scramtoni@gmail.com>
2022-03-04 12:45:56 +0000

    with new tree and plock

---
+++ pages/dob.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dob from '../component/Dob'
+
+const some = () => {
+    return (
+        <div>
+            <Dob />
+        </div>
+    )
+}
+
+export default some
