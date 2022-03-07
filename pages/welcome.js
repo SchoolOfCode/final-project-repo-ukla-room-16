cb22505672c01d22a796f21aef823f26c0f7a670
Tonisaraiva <scramtoni@gmail.com>
2022-03-07 11:23:35 +0000

    welcome page start

---
+++ pages/welcome.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const welcomePage = () => {
+    return (
+        <div>
+            <H1>Bringing the Familia together under one app</H1>
+
+            <p> Familia is your family app, where you have a safe and secure space to discuss and share information with your close family only.
+            Other social media platform might feel heavy with too many options, too many people sharing too much you do not necessarily care about...
+
+            With Familia we want to bring the people you care about closer to you!
+
+            How many times did you have to ask your daughter for the size of shoes of your grandson? Has your son ever asked you about the postal address of his sister? Do you never remember what your husband blood group is? Ever asked your wife what is the birth date of your father-in-Law?
+
+            Familia will centralise these snippets of information for you and for your close family to know.
+
+            The app also offers a secure place where you can exchange messages and pictures with all the family members. No need of various cloud plaform, exchanging pictures over Messenger and having to share that individually with different people.
+
+            When we think about family we think about family tree... we also offer you the opportunity to visualise your family and grow your family tree with our innovative family tree display.
+
+            More to come!
+            - Your Familia recipe book. One place to keep these precious family recipes
+            - The Familia events, with a calendar where you can plan and be reminded of important family events, gatherings, birthdays etc.
+            - The familia family tracker will help you keep a peaceful mind, knowing where your children are any time 
+
+
+            </p>
+        </div>
+    )
+}
+
+export default welcomePage
