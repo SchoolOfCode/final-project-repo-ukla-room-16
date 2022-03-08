79b984bb7f42c6d6aeb4d99cdd8174a2527f832f
kevweolomu <kevweolomu@yahoo.ca>
2022-03-08 16:37:15 +0000

    welcome page styling ongoing

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,40 +1,40 @@
-import React from 'react';
 import logofamilia from "../images/Familia9.png";
 import Image from 'next/image';
 import tree from "../images/tree.png";
 import meal from "../images/meal.png";
 import frontimage from "../images/familia_frontpage_image.jpg";
-import styles from "../styles/Welcome.module.css"
+import styles from "../styles/Welcome.module.css";
+import eventplanner from "../images/eventplanner.jpeg";
+import peace from "../images/peace.jpeg";
+import famrep from "../images/famrep.jpeg";
 
 const WelcomePage = () => {
     return (
-      <>
+      <div className={styles.entire}>
       <div>
-        <div >
+        <div>
         <div>
         <Image src={logofamilia} width="100px" height="100px" />
         </div>
 
         <div className={styles.familyimage}>
-            <h1 >Bringing the Familia 
-            <br></br>together under one app</h1>
+            <h1 >Bringing the Family 
+            <br></br>together in one app</h1>
             <Image src={frontimage}
           alt="Picture of a family from grandparents, to children and grandchildren"
           width="150px"
-          height="75px"/>
+          height="75px" />
           </div>
-
-            
+<br></br>
+<br></br>
             <div className={styles.container}>
             <div className={styles.leftcolumn}></div> 
             
             <div className={styles.middlecolumn}>
-            <div className={styles.text}> Familia is your family app, where you have a safe and secure space to discuss and share information with your close family only.
-            </div>
-
-            <br></br>
-            <br></br>
-            <p>
+            <div className={styles.text}> <p>Familia is your family app, where you have a safe and secure space to discuss and share information with your close family only.
+            
+           <br></br>
+           <br></br>
             Other social media platforms might feel heavy with too many options, too many people sharing too much you do not necessarily care about...
             <br></br>
             <br></br>
@@ -47,43 +47,48 @@ const WelcomePage = () => {
             Familia will centralise these snippets of information for you and for your close family to know.
             </p>
             </div>
+            </div>
            </div>
-           <div>
+<br></br>
+           <div className={styles.secure}>
            {/* <Image insert image of posts or feed/> */}
            <p>
             The app also offers a secure place where you can exchange messages and pictures with all the family members. No need for various cloud platsform, exchanging pictures over Messenger and having to share that with people outside your family or inner circle.
             </p></div>
             </div>
+
             <div className={styles.rightcolumn}></div>
-                        
-            <div>
-            <Image src={tree} width="400px" height="400px"/>
+            
+            <div className={styles.tree}>
+            
+            <Image src={tree} width="400px" height="400px" />
+            
             <p>When we think about family trees... we also offer you the opportunity to visualise your family and grow your family tree with our innovative family tree display.</p>
             </div>
-
-                    
-            <div className="more">
-            <p>More to come!</p>
-            
-            <div>
-            <Image src={meal} width="85px" height="90px"/>
+<br></br>
+<br></br>
+<p style={{fontSize: "25px", fontWeight: "bolder", color: "Red"}}>More to come!</p>        
+            <div className={styles.more}>
+                        
+            <div classname={styles.recipe}>
+            <Image src={famrep} width="100px" height="100px" />
             <p>Your Familia recipe book. One place to keep these precious family recipes</p>
             </div>
 
-            <div>
-            {/* <Image insert an event logo or image/> */}
+            <div className={styles.events}>
+            <Image src={eventplanner} width="85px" height="90px"/>
             <p>The Familia events, with a calendar where you can plan and be reminded of important family events, gatherings, birthdays etc.</p>
             </div>
            
-           <div>
-           {/* <Image insert image of peacful family/> */}
+           <div className={styles.others}>
+           <Image src={peace} width="250px" height="100px"/>
            <p>The familia family app will help you keep a peaceful mind, knowing where your children are any time 
            </p>
            </div>
             </div>
             </div>
-            
-      </>
+            </div>            
+      
     )
 }
 
