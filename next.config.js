648a2234f742d3d468573df6b037337cbf72d1e5
Tonisaraiva <scramtoni@gmail.com>
2022-03-10 05:31:22 +0000

    header commented out

--- next.config.js
+++ next.config.js
@@ -20,49 +20,49 @@ module.exports = withPlugins([withTM], nextConfig);
 
 
 
-module.exports = {
-  // append this at the bottom of your next.config.js file
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-      {
-        key: 'X-DNS-Prefetch-Control',
-        value: 'on',
-      },
-      {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=63072000; includeSubDomains; preload',
-      },
-      {
-        key: 'Server',
-        value: 'Apache', // phony server value
-      },
-      {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff',
-      },
-      {
-        key: 'X-Frame-Options',
-        value: 'sameorigin',
-      },
-      {
-        key: 'X-XSS-Protection',
-        value: '1; mode=block',
-      },
-      {
-        key: 'Referrer-Policy',
-        value: 'same-origin',
-      },
-      {
-        key: 'Permissions-Policy',
-        value: 'geolocation=*', // allow specified policies here
-      },
-    ]
-  },
+// module.exports = {
+//   // append this at the bottom of your next.config.js file
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)',
+//         headers: [
+//       {
+//         key: 'X-DNS-Prefetch-Control',
+//         value: 'on',
+//       },
+//       {
+//         key: 'Strict-Transport-Security',
+//         value: 'max-age=63072000; includeSubDomains; preload',
+//       },
+//       {
+//         key: 'Server',
+//         value: 'Apache', // phony server value
+//       },
+//       {
+//         key: 'X-Content-Type-Options',
+//         value: 'nosniff',
+//       },
+//       {
+//         key: 'X-Frame-Options',
+//         value: 'sameorigin',
+//       },
+//       {
+//         key: 'X-XSS-Protection',
+//         value: '1; mode=block',
+//       },
+//       {
+//         key: 'Referrer-Policy',
+//         value: 'same-origin',
+//       },
+//       {
+//         key: 'Permissions-Policy',
+//         value: 'geolocation=*', // allow specified policies here
+//       },
+//     ]
+//   },
 
 
-    ];
-  },
-};
+//     ];
+//   },
+// };
