59cf49fc63c506f13118d4a44acdbfba9df8fff2
Tonisaraiva <scramtoni@gmail.com>
2022-03-10 08:27:28 +0000

    hamburger implementation in progress

---
+++ hamburger/Menu/Menu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { bool } from 'prop-types';
+import { StyledMenu } from './Menu.styled';
+
+const Menu = ({ open, ...props }) => {
+  
+  const isHidden = open ? true : false;
+  const tabIndex = isHidden ? 0 : -1;
+
+  return (
+    <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
+      <a href="/welcome" tabIndex={tabIndex}>
+        <span aria-hidden="true"></span>
+        About the App
+      </a>
+      <a href="/welcome#quickstart" tabIndex={tabIndex}>
+        <span aria-hidden="true"></span>
+        Quickstart
+        </a>
+      <a href="/welcome#AboutUs" tabIndex={tabIndex}>
+        <span aria-hidden="true"></span>
+        Who we are
+        </a>
+    </StyledMenu>
+  )
+}
+
+Menu.propTypes = {
+  open: bool.isRequired,
+}
+
+export default Menu;
